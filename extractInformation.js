45041ef003122d09bc2df28a9e2d7f0c517c6538
Yunus Kerem Turk <krmtrk20@gmail.com>
2020-03-11 13:34:30 +0300

    Working version.

--- extractInformation.js
+++ extractInformation.js
@@ -24,17 +24,20 @@ selectFile.addEventListener('change', handleFileSelect, false);
 
 listElement.addEventListener("change",function(event){
     //case for 29 dayed FEB.
-    if(listElement.options[listElement.selectedIndex].value === 28)
+    if(parseInt(listElement.options[listElement.selectedIndex].value) === 28)
     {
-        var date = new Date().getFullYear;
-        if(date % 4 === 0)
+        var date = parseInt(new Date().getFullYear());
+        
+        if(date % 4 == 0)
         {
             selectedMonthDays = 29;
         }
     }
-    selectedMonthDays =  listElement.options[listElement.selectedIndex].value
+    else{
+        selectedMonthDays =  parseInt(listElement.options[listElement.selectedIndex].value);
+    }
     
-    if(items != undefined){
+    if(items.length > 0){
         dynamic.innerHTML = "";
         findNumberOfPeople(items);
     }
@@ -129,7 +132,8 @@ function prepareData(resultString, fileName) {
                 projectDescription: rowData[9],
                 projectUrl: rowData[10],
                 storyPoints: rowData[11],
-                wp: rowData[12]
+                wp: rowData[12],
+                timeSpent: rowData[13]
             };
             items.push(item);
         }
@@ -149,7 +153,7 @@ function findNumberOfPeople(items){
             personArr.push(
                 {
                     name: items[i].assignee,
-                    days: new Array(selectedMonthDays)
+                    days: new Array(selectedMonthDays).fill(true)
                 }
                 )
             nameArr.push(items[i].assignee)
@@ -178,14 +182,23 @@ function renderDaysAndButton()
         divPerson.appendChild(divCheckBox);
 
         dynamic.appendChild(divPerson);
-
+        var date = new Date();
         for(var j = 1; j <= selectedMonthDays; j++)
         {
             var checkbox = document.createElement('input');
             checkbox.style = 'margin-left: 2px;'
             checkbox.type = "checkbox";
             checkbox.value = j;
-            checkbox.checked = true;
+            var curDate = new Date(date.getFullYear(),listElement.selectedIndex, j);
+            //Make saturdays and sundays unchecked.
+            if(curDate.getDay() == 0 || curDate.getDay() == 6)
+            {
+                checkbox.checked = false;
+            }
+            else
+            {
+                checkbox.checked = true;
+            }
             checkbox.textContent = i;
             var id = j + "_" + i;
 
@@ -206,56 +219,146 @@ function renderDaysAndButton()
 function addListeners()
 {
     var checkboxes = document.querySelectorAll("input[type=checkbox]");
-    console.log(checkboxes);
     for(var i = 0; i < checkboxes.length; i++)
     {
+        if(checkboxes[i].checked === false)
+        {
+            personArr[Number(checkboxes[i].textContent)].days[Number(checkboxes[i].value) - 1] = false;
+        }
+
         checkboxes[i].addEventListener("change", function(){
             if(!this.checked)   //if unchecked, person was not present that day.
             {
-            personArr[Number(this.textContent)].days[Number(this.value) - 1] = false;
+                personArr[Number(this.textContent)].days[Number(this.value) - 1] = false;
             }
         })
     }
 }
 
 
-function generateOutput(items) 
+async function generateOutput(items) 
 {
-    var groupByProject = items.reduce((r,a) => {
+    var groupByProject = await items.reduce((r,a) => {
         r[a.projectName] = [...r[a.projectName] || [], a]
         return r
     },[])
-    
-    for(var project in groupByProject)
-    {
-        if(groupByProject.hasOwnProperty(project))
-        {
-            var groupByAssigneeForProject = groupByProject[project].reduce((r,a) => {
-                r[a.assignee] = [...r[a.assignee] || [], a]
-                return r 
-            }, [])
-            for(var assignee in groupByAssigneeForProject)
-            {
-                if(groupByAssigneeForProject.hasOwnProperty(assignee))
-                {
-                    var groupByWorkPackageForAssignee = groupByAssigneeForProject[assignee].reduce((r,a) => {
-                        r[a.wp] = [...r[a.wp] || [], a];
-                        return r;
-                    })
 
-                    for(var workpackage in groupByWorkPackageForAssignee)
+    var defaultWorkbook = new Excel.Workbook();
+    var defaultWorksheet;
+    defaultWorkbook.xlsx.readFile("excelFiles/rawFiles/H2020_timesheet_template.xlsx").then(async function(){
+        defaultWorksheet = defaultWorkbook.getWorksheet(1);
+
+        for(var project in groupByProject)
+        {
+            if(groupByProject.hasOwnProperty(project))
+            {
+                var groupByAssigneeForProject = await groupByProject[project].reduce((r,a) => {
+                    r[a.assignee] = [...r[a.assignee] || [], a]
+                    return r 
+                }, [])
+                for(var assignee in groupByAssigneeForProject)
+                {
+                    var workbook = new Excel.Workbook;
+                    var sheet = workbook.addWorksheet();
+                    defaultWorksheet.eachRow((row, rowNumber) => {
+                        var newRow = sheet.getRow(rowNumber);
+                        row.eachCell((cell, colNumber) => {
+                            var newCell = newRow.getCell(colNumber)
+                            for(var prop in cell){
+                                newCell[prop] = cell[prop];
+                            }
+                        })
+                    })            
+                    if(groupByAssigneeForProject.hasOwnProperty(assignee))
                     {
-                        if(groupByWorkPackageForAssignee.hasOwnProperty(workpackage))
+                        var groupByWorkPackageForAssignee = await groupByAssigneeForProject[assignee].reduce((r,a) => {
+                            r[a.wp] = [...r[a.wp] || [], a];
+                            return r;
+                        }, []);
+                        var rowNumberForWorkPackage = 6;
+                        
+                        for(var workpackage in groupByWorkPackageForAssignee)
                         {
-                            //write to excel now!
+                            if(groupByWorkPackageForAssignee.hasOwnProperty(workpackage))
+                            {
+                                var totalTimeSpentOnWP = 0.0;
+                                for (var i = 0; i < groupByWorkPackageForAssignee[workpackage].length; i++)
+                                {
+                                    if(groupByWorkPackageForAssignee[workpackage][i].status === "Done")
+                                    {
+                                        if(groupByWorkPackageForAssignee[workpackage][i].timeSpent != "" && groupByWorkPackageForAssignee[workpackage][i].timeSpent != null && groupByWorkPackageForAssignee[workpackage][i].timeSpent != undefined)
+                                        {
+                                            if (parseInt(groupByWorkPackageForAssignee[workpackage][i].timeSpent) > 0)   {
+                                                totalTimeSpentOnWP += parseInt(groupByWorkPackageForAssignee[workpackage][i].timeSpent);
+                                            }
+                                        }
+                                    }
+                                }
+                                totalTimeSpentOnWP = totalTimeSpentOnWP / 3600;
+                                var totalTimeSpentPerDay = [];
+
+                                if(totalTimeSpentOnWP > 0)
+                                {
+                                    var sheetRow = sheet.getRow(rowNumberForWorkPackage);
+                                    sheet.getCell(rowNumberForWorkPackage,1).text = workpackage;
+                                    
+                                    for(var t = 0; t < personArr.length; t++){
+                                        if(personArr[t].name === assignee){
+                                            for(var n = 0; n < personArr[t].days.length; n++){
+                                                //last element has been reached.
+                                                if(n == personArr[t].days.length - 1){
+                                                    while (totalTimeSpentOnWP > 0) {
+                                                        var hours = Math.round(Math.random()*n);
+
+                                                        if(hours < 5)
+                                                        {
+                                                            totalTimeSpentPerDay.push(hours);
+                                                            totalTimeSpentOnWP -= hours;
+                                                        }
+                                                        
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                    for(var t = 0; t < personArr.length; t++){
+                                        if(personArr[t].name === assignee){
+                                            for(var n = 0; n < personArr[t].days.length; n++){
+                                                //burda da arrayden okuyup islem yap.
+                                                if(personArr[t].days[n] != false){
+                                                    
+                                                    var columnSum = 0;
+                                                    sheet.getColumn(n+2).eachCell((cell,rowNumber) => {
+                                                        if(rowNumber >= 6 && rowNumber < rowNumberForWorkPackage){
+                                                            columnSum += cell.value;
+                                                        }
+                                                    });
+                                                    if(columnSum + totalTimeSpentPerDay[totalTimeSpentPerDay.length - 1] > 6)
+                                                    {
+                                                        sheetRow.getCell(n+2).value = 0;
+                                                    }
+                                                    else
+                                                    {
+                                                        sheetRow.getCell(n+2).value = totalTimeSpentPerDay.pop();
+                                                    }
+                                                    
+                                                }
+                                            }
+                                        }
+                                    }
+                                    console.log(rowNumberForWorkPackage + " " + assignee + " " + project + " " + workpackage);
+                                    rowNumberForWorkPackage++;
+                                    totalTimeSpentOnWP = 0;
+                                }
+                            }
                         }
                     }
+                    workbook.xlsx.writeFile("excelFiles/processedFiles/" + project.toString().substring(0,project.toString().indexOf("/")) + project.toString().substring(project.toString().indexOf("/") + 1) + "_" + assignee + ".xlsx").then(function(){});
                 }
             }
         }
-    }
-
-
+    });
+    alert("Documents have been created.")
 }
 
 function clearFile() {
@@ -269,6 +372,7 @@ function clearFile() {
     dynamic.innerHTML = "";
     donwloadRow.innerHTML = "";
     unassigned.innerHTML = "";
+    continueButton.style = "display:none" ;
 }
 
 // helper functions
